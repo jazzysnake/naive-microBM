ea3d319f61d53135ada9cefe5289e167d50c8b27
jazzysnake <dotya77@gmail.com>
2022-10-13 14:28:15 +0200

    added func to transact with 2 pockets at once

--- src/main/java/org/example/DemoAssetContract.java
+++ src/main/java/org/example/DemoAssetContract.java
@@ -13,21 +13,13 @@ import org.hyperledger.fabric.contract.annotation.Info;
 import org.hyperledger.fabric.contract.annotation.License;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-@Contract(name = "DemoAssetContract",
-    info = @Info(title = "DemoAsset contract",
-                description = "My Smart Contract",
-                version = "0.0.1",
-                license =
-                        @License(name = "Apache-2.0",
-                                url = ""),
-                                contact =  @Contact(email = "naive-contract@example.com",
-                                                name = "naive-contract",
-                                                url = "http://naive-contract.me")))
+@Contract(name = "DemoAssetContract", info = @Info(title = "DemoAsset contract", description = "My Smart Contract", version = "0.0.1", license = @License(name = "Apache-2.0", url = ""), contact = @Contact(email = "naive-contract@example.com", name = "naive-contract", url = "http://naive-contract.me")))
 @Default
 public class DemoAssetContract implements ContractInterface {
-    public  DemoAssetContract() {
+    public DemoAssetContract() {
 
     }
+
     @Transaction()
     public boolean demoAssetExists(Context ctx, String demoAssetId) {
         byte[] buffer = ctx.getStub().getState(demoAssetId);
@@ -36,9 +28,9 @@ public class DemoAssetContract implements ContractInterface {
 
     @Transaction()
     public void createDemoAsset(Context ctx, String uuid, Double pocket1, Double pocket2, Double pocket3) {
-        boolean exists = demoAssetExists(ctx,uuid);
+        boolean exists = demoAssetExists(ctx, uuid);
         if (exists) {
-            throw new RuntimeException("The asset "+uuid+" already exists");
+            throw new RuntimeException("The asset " + uuid + " already exists");
         }
         DemoAsset asset = new DemoAsset();
         asset.setUuid(uuid);
@@ -50,44 +42,52 @@ public class DemoAssetContract implements ContractInterface {
 
     @Transaction()
     public DemoAsset readDemoAsset(Context ctx, String demoAssetId) {
-        boolean exists = demoAssetExists(ctx,demoAssetId);
+        boolean exists = demoAssetExists(ctx, demoAssetId);
         if (!exists) {
-            throw new RuntimeException("The asset "+demoAssetId+" does not exist");
+            throw new RuntimeException("The asset " + demoAssetId + " does not exist");
         }
 
-        DemoAsset newAsset = DemoAsset.fromJSONString(new String(ctx.getStub().getState(demoAssetId),UTF_8));
+        DemoAsset newAsset = DemoAsset.fromJSONString(new String(ctx.getStub().getState(demoAssetId), UTF_8));
         return newAsset;
     }
 
     @Transaction()
-    public void transactWithPocket1(Context ctx, String uuid, Double pocket1){
+    public void transactWithPocket1(Context ctx, String uuid, Double pocket1) {
         DemoAsset asset = readDemoAsset(ctx, uuid);
-        if((asset.getPocket1()+pocket1)>=0){
-            updateDemoAsset(ctx, uuid, asset.getPocket1()+pocket1, null, null);
+        if ((asset.getPocket1() + pocket1) >= 0) {
+            updateDemoAsset(ctx, uuid, asset.getPocket1() + pocket1, null, null);
         }
     }
 
     @Transaction()
-    public void transactWithPocket2(Context ctx, String uuid, Double pocket2){
+    public void transactWithPocket2(Context ctx, String uuid, Double pocket2) {
         DemoAsset asset = readDemoAsset(ctx, uuid);
-        if((asset.getPocket2()+pocket2)>=0){
-            updateDemoAsset(ctx, uuid, null, asset.getPocket2()+pocket2,null);
+        if ((asset.getPocket2() + pocket2) >= 0) {
+            updateDemoAsset(ctx, uuid, null, asset.getPocket2() + pocket2, null);
         }
     }
 
     @Transaction()
-    public void transactWithPocket3(Context ctx, String uuid, Double pocket3){
+    public void transactWithPocket3(Context ctx, String uuid, Double pocket3) {
         DemoAsset asset = readDemoAsset(ctx, uuid);
-        if((asset.getPocket3()+pocket3)>=0){
-            updateDemoAsset(ctx, uuid, null,null,asset.getPocket3()+pocket3);
+        if ((asset.getPocket3() + pocket3) >= 0) {
+            updateDemoAsset(ctx, uuid, null, null, asset.getPocket3() + pocket3);
         }
     }
 
     @Transaction()
-    public void updateDemoAsset(Context ctx, String uuid, Double pocket1,Double pocket2, Double pocket3) {
-        boolean exists = demoAssetExists(ctx,uuid);
+    public void transactWithPocket23(Context ctx, String uuid, Double pocket2, Double pocket3) {
+        DemoAsset asset = readDemoAsset(ctx, uuid);
+        if ((asset.getPocket3() + pocket3) >= 0 && (asset.getPocket2() + pocket2) >= 0) {
+            updateDemoAsset(ctx, uuid, null, asset.getPocket2() + pocket2, asset.getPocket3() + pocket3);
+        }
+    }
+
+    @Transaction()
+    public void updateDemoAsset(Context ctx, String uuid, Double pocket1, Double pocket2, Double pocket3) {
+        boolean exists = demoAssetExists(ctx, uuid);
         if (!exists) {
-            throw new RuntimeException("The asset "+uuid+" does not exist");
+            throw new RuntimeException("The asset " + uuid + " does not exist");
         }
         DemoAsset asset = readDemoAsset(ctx, uuid);
         if (pocket1 != null) {
@@ -105,9 +105,9 @@ public class DemoAssetContract implements ContractInterface {
 
     @Transaction()
     public void deleteDemoAsset(Context ctx, String demoAssetId) {
-        boolean exists = demoAssetExists(ctx,demoAssetId);
+        boolean exists = demoAssetExists(ctx, demoAssetId);
         if (!exists) {
-            throw new RuntimeException("The asset "+demoAssetId+" does not exist");
+            throw new RuntimeException("The asset " + demoAssetId + " does not exist");
         }
         ctx.getStub().delState(demoAssetId);
     }
